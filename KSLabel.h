d4e6e263c5c80776ad710cec722d46894c1ef07f
Keith Smiley <keithbodesmiley@gmail.com>
2012-11-15 16:22:17 -0500

    Added comments

--- KSLabel.h
+++ KSLabel.h
@@ -1,6 +1,6 @@
 //
 //  KSLabel.h
-//  Test
+//  A NSTextField subclass implementing label-like functionality
 //
 //  Created by Keith Smiley on 11/14/12.
 //  Copyright (c) 2012 Keith Smiley. All rights reserved.
@@ -10,8 +10,21 @@
 
 @interface KSLabel : NSTextField
 
+/**
+ The name of an `NSNotification` you want the label to post every time it is clicked
+ 
+ EX: [myLabel setNotificationToPost:labelClicked];
+ **/
 @property (nonatomic, strong) NSString *notificationToPost;
 
+/**
+ Init with an attributed string configured how you want the text to be displayed
+ 
+ Pass the view you will add the label to. The label will be sized to fit
+   and the frame will be setup to be centered in the view
+ 
+ Then set a notification if you'd like and call [someView addSubview:myLabel];
+ **/
 - (id)initWithAttributedString:(NSAttributedString *)string inView:(NSView *)view;
 
 @end
--- KSLabel.m
+++ KSLabel.m
@@ -12,8 +12,10 @@
 
 - (id)initWithAttributedString:(NSAttributedString *)string inView:(NSView *)view
 {
+    // The hight and width of the string
     NSSize labelSize = string.size;
     
+    // Setup the frame to be the center of the passed view
     NSRect labelFrame = NSMakeRect(NSMidX(view.bounds) - (labelSize.width / 2.0f),
                                    NSMidY(view.bounds) - (labelSize.height / 2.0f),
                                    ceil(labelSize.width),
@@ -24,17 +26,21 @@
         return nil;
     }
 
+    // Sets the string value and disables `NSTextField` like attributes
     [self setAttributedStringValue:string];
     [self setBezeled:NO];
     [self setDrawsBackground:NO];
     [self setEditable:NO];
     [self setSelectable:NO];
+    
+    // Causes the label to fit to the text, shouldn't change significantly
     [self sizeToFit];
     
     return self;
 }
 
-- (void)mouseDown:(NSEvent *)theEvent {
+- (void)mouseUp:(NSEvent *)theEvent {
+    // If the notification string is set post the notification on mouseUp
     if (self.notificationToPost && ![self.notificationToPost isEqualToString:@""]) {
         [[NSNotificationCenter defaultCenter] postNotificationName:self.notificationToPost
                                                             object:nil];
